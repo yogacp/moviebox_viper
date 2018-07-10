244831654e1be41ee768f6ee567b6d63bb160cf3
yogacp <ichi.emo@gmail.com>
2018-07-10 15:02:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# moviebox_kotlin
+Listing movie box from https://www.themoviedb.org using Kotlin and MVP
---
+++ app/src/main/java/com/moviebox/viper/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.moviebox.viper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
